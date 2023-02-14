2cfca34706db25f377cb6fb3a357ba4d81bfaa68
k-kazuma <k_kazuma8@icloud.com>
2023-02-14 14:20:44 +0900

    フォーム作成開始

--- src/cp/Contact.jsx
+++ src/cp/Contact.jsx
@@ -1,68 +1,16 @@
 import React from 'react'
 import Menu from './Menu'
 import { useState } from 'react'
-import {init, send} from '@emailjs/browser'
+// import {init, send} from '@emailjs/browser'
 
 function Contact() {
 
-  const [company, setCompany] = useState('')
-  const [name, setName] = useState('')
-  const [name2, setName2] = useState('')
-  const [phon, setPhon] = useState('')
-  const [mail, setMail] = useState('')
-  const [message, setMessage] = useState('')
+  const initialValues = {company: "", username: "", username2: "", phon: "", mail: "", massege: ""};
+  const [formValues, setFormValues] = useState(initialValues);
 
-  const contactTitle = [
-    {
-        name: '会社名 / 屋号',
-        id:'f1',
-        set: setCompany,
-        val: company
-    },
-    {
-        name: '氏名',
-        id:'f2',
-        set: setName,
-        val: name
-    },
-    {
-        name: 'フリガナ',
-        id:'f3',
-        set: setName2,
-        val: name2
-    },
-    {
-        name: '電話番号',
-        id:'f4',
-        set: setPhon,
-        val:phon
-    },
-    {
-        name: 'メールアドレス',
-        id:'f5',
-        set: setMail,
-        val: mail
-    },
-  ]
-
-  const clickSendMail = () => {
-
-    init("FTYyGYT9tu65HVkjk");
-
-    const templateVariables = {
-      company: company,
-      user_name: name,
-      user_name2: name2,
-      user_email: mail,
-      user_phon: phon,
-      message: message,
-    };
-  
-    send(
-      'service_5f876ur',
-      'template_pgncwcg',
-      templateVariables
-    );
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({...formValues, [name]: value});
   }
 
 
@@ -72,38 +20,36 @@ function Contact() {
     <>
       <div className='header'>
           <div className="container">
-              <div className="title">Contact</div>
-              <form>
-                {
-                  contactTitle.map((title, key) => {
-                    return (
-                        <div key={key}>
-                          <label htmlFor={title.id}>{title.name}</label>
-                          <input 
-                          type="text" 
-                          id={title.id} 
-                          value={title.val}
-                          className='fromInput' 
-                          onChange={(e) => title.set(e.target.value)}
-                          />
-                        </div>
-                    )
-                  }) 
-                }
-                <label htmlFor="area">問い合わせ内容</label>
-                <textarea 
-                id="area" 
-                cols="30" 
-                rows="5"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                />
-                <button
-                onClick={clickSendMail}
-                >
-                  送信
-                </button>
-              </form>
+              <h2 className="title">Contact</h2>
+              <div className="formContainar">
+                <form>
+                  <div className='formField'>
+                    <label>会社名</label>
+                      <input type="text" name='company' className='formInput' onChange={(e) => handleChange(e)} />
+                  </div>
+                  <div className='formField'>
+                    <label>担当者名</label>
+                      <input type="text" name='username' className='formInput' onChange={(e) => handleChange(e)} />
+                  </div>
+                  <div className='formField'>
+                    <label>担当者名（フリガナ）</label>
+                      <input type="text" name='username2' className='formInput' onChange={(e) => handleChange(e)} />
+                  </div>
+                  <div className='formField'>
+                    <label>連絡先電話番号</label>
+                      <input type="text" name='phon' className='formInput' onChange={(e) => handleChange(e)} />
+                  </div>
+                  <div className='formField'>
+                    <label>連絡先メールアドレス</label>
+                      <input type="text" name='mail' className='formInput' onChange={(e) => handleChange(e)} />
+                  </div>
+                  <div className='formField'>
+                    <label>問い合わせ内容</label>
+                    <textarea name='message' className='formArea' id="area" cols="30" rows="5"/>
+                  </div>
+                  <button className='submitButton'>送信</button>
+                </form>
+              </div>
           </div>
         <Menu />
       </div>
